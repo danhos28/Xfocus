12d14e44b8af12dd228844714c9410eccf65a65a
danhos28 <danhos2810@gmail.com>
2020-10-26 19:50:49 +0700

    add scrollview

--- app/src/main/java/com/example/xfocus/StartPages/Login.java
+++ app/src/main/java/com/example/xfocus/StartPages/Login.java
@@ -10,19 +10,19 @@ import android.widget.ImageView;
 
 import com.example.xfocus.R;
 
-public class ClientNo extends AppCompatActivity {
+public class Login extends AppCompatActivity {
     ImageView xfocuslogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clientno);
+        setContentView(R.layout.activity_login);
 
         xfocuslogo = findViewById(R.id.xfocus);
         ((Animatable) xfocuslogo.getDrawable()).start();//start Animation
     }
 
-    public void ClientNo(View view) {
-        Intent intent = new Intent(ClientNo.this, Login.class);
+    public void AnotherAcc(View view) {
+        Intent intent = new Intent(Login.this, ClientNo.class);
         startActivity(intent);
         finish();
     }
