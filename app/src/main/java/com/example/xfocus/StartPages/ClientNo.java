0d17689351ec508ffa9e94e141371fec1ab4098f
AdmiralRX <stefenradeon@gmail.com>
2020-10-29 09:12:22 +0700

    aaa

--- app/src/main/java/com/example/xfocus/StartPages/ClientNo.java
+++ app/src/main/java/com/example/xfocus/StartPages/ClientNo.java
@@ -5,13 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.xfocus.R;
 
 public class ClientNo extends AppCompatActivity {
     ImageView xfocuslogo;
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    //Controlling the back button
+    @Override
+    public void onBackPressed() {
+        //For back pressed twice for quit
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            finishAffinity();
+            System.exit(0);
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please tap BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +50,7 @@ public class ClientNo extends AppCompatActivity {
         ((Animatable) xfocuslogo.getDrawable()).start();//start Animation
     }
 
+    //On client no submitted
     public void ClientNo(View view) {
         Intent intent = new Intent(ClientNo.this, Login.class);
         startActivity(intent);
--- app/src/main/java/com/example/xfocus/StartPages/Login.java
+++ app/src/main/java/com/example/xfocus/StartPages/Login.java
@@ -5,14 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.xfocus.R;
 
 public class Login extends AppCompatActivity {
     ImageView xfocuslogo;
-    int aaa;
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    //Controlling the back button
+    @Override
+    public void onBackPressed() {
+        //For back pressed twice for quit
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            finishAffinity();
+            System.exit(0);
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please tap BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
