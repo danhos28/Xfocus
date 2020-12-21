5ca46b607c06d850aac2b860642a08ca262150cb
danhos28 <danhos2810@gmail.com>
2020-12-21 17:42:16 +0700

    add HutangPiutang, PendapatanBiaya

--- app/src/main/java/com/example/xfocus/PendapatanBiaya.java
+++ app/src/main/java/com/example/xfocus/PendapatanBiaya.java
@@ -1,4 +1,29 @@
 package com.example.xfocus;
 
+import java.util.ArrayList;
+
 public class PendapatanBiaya {
+    private static String Label,Tipe,Value;
+    private static ArrayList<String> ListPendapatanBiaya = new ArrayList();
+    public PendapatanBiaya(String label, String tipe, String value, ArrayList<String>listPendapatanBiaya){
+        this.Label = label;
+        this.Tipe = tipe;
+        this.Value = value;
+        this.ListPendapatanBiaya = listPendapatanBiaya;
+    }
+    public static String getLabel(){ return Label; }
+    public static void setLabel(String label){
+        Label = label;
+    }
+    public static String getTipe(){ return Tipe; }
+    public static void setTipe(String tipe){
+        Tipe = tipe;
+    }
+    public static String getValue(){ return Value; }
+    public static void setValue(String value){
+        Value = value;
+    }
+    public static ArrayList<String> getListPendapatanBiaya() { return ListPendapatanBiaya; }
+    public static void setListPendapatanBiaya(ArrayList<String> listPendapatanBiaya) { ListPendapatanBiaya = listPendapatanBiaya; }
 }
+
